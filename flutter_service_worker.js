d339f1848a1571a68531e3df9e6b17f454533ac7
Ilham Fadilah <ilham@ozip.co.id>
2023-05-06 20:33:02 +0700

    [MIF] Revisi tanggal

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e4e816fcb85a7338e249c0053fa8b740",
-"/": "e4e816fcb85a7338e249c0053fa8b740",
-"main.dart.js": "0b9b9998f0b3cf12447a6d6946c7d337",
+"index.html": "fdb233be0f01b03547c48e4c2998a86d",
+"/": "fdb233be0f01b03547c48e4c2998a86d",
+"main.dart.js": "424be886dadff7578b3ebc26b7e71585",
 "manifest.json": "4c35f39cc67c647b7adf40a31febea51",
 "version.json": "3db1808c47ce8f4207308a9d45b5aa74"
 };
