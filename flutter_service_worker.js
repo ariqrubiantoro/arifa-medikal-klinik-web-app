cfd676722d2384fa162b7e24bc9cf4096350b5aa
Ilham Fadilah <ilham@ozip.co.id>
2023-05-23 10:51:53 +0700

    revisi bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3db1808c47ce8f4207308a9d45b5aa74",
-"index.html": "31eebaa66ee893ae383f461f8b111871",
-"/": "31eebaa66ee893ae383f461f8b111871",
-"main.dart.js": "45b10bca03dc5b210110ca472e282651",
+"index.html": "e26fbcfaf2e5e5282e98ac7ad4c14e48",
+"/": "e26fbcfaf2e5e5282e98ac7ad4c14e48",
+"main.dart.js": "0f20a7fe699abe9648d1804a7edc1ddb",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "8ddd5f105a59494177502a787bb3979b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
