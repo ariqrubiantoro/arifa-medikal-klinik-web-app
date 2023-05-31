6fc956c22c388ccacbaf8cd4511e62e8c28a3e6f
Ilham Fadilah <ilham@ozip.co.id>
2023-06-01 05:48:47 +0700

    cardiovaskuler

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3db1808c47ce8f4207308a9d45b5aa74",
-"index.html": "0348e758874150a8475d7a1fac5fe6df",
-"/": "0348e758874150a8475d7a1fac5fe6df",
-"main.dart.js": "a7d06a0672c3c414970a6fbd940db854",
+"index.html": "7d6662d0a76dc6e6337507c41a5ba10b",
+"/": "7d6662d0a76dc6e6337507c41a5ba10b",
+"main.dart.js": "565f37625f86dcce6c22166fad5d704a",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "8ddd5f105a59494177502a787bb3979b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
